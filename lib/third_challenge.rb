a1441b94b4502d6035e7ca765467a97a1731afce
Daniel Shimield <dxshim90@gmail.com>
2019-01-31 13:30:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,23 +13,23 @@ def third_challenge
         :name => "Romeo",
         :age => "15",
         :status => "alive"
-        }
+        },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord capulet"
+        :name => "Lord Capulet",
         :age => "50"
-        }
+        },
       :matriarch => {
-        :name => "Lady capulet"
+        :name => "Lady Capulet",
         :age => "51"
-        }
+        },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
-        }
+        },
       :heroine_friends => []
    }
   }
