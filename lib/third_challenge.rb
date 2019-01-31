1e83aebac0bbe5354a03955cc5f12fd97738bcf7
Daniel Shimield <dxshim90@gmail.com>
2019-01-31 13:23:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name =>
+        :name => "Lord Montague"
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
+        :age => ""
         },
       :hero_friends => []
    }, 
